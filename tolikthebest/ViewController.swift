af7abafbd43cee18565b467543fd1f498cf7eb2e
treningDISNEY <118128250+treningDISNEY@users.noreply.github.com>
2022-11-13 21:46:46 +0300

    work5

--- tolikthebest/ViewController.swift
+++ tolikthebest/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
+        view.backgroundColor = .blue
+        
     }
 
 
